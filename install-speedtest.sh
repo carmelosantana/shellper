68567f93f648a45888bd3763989cd4a5f4d61013
Carmelo Santana <me@carmelosantana.com>
2017-03-08 00:10:23 -0500

    Added accept install Cleaned up y/n

--- install-speedtest.sh
+++ install-speedtest.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # Install Speedtest CLI
 # https://fossbytes.com/test-internet-speed-linux-command-line/
 echo -n "Install + run Speedtest CLI? (y|n)"
 read answer
-if echo "$answer" | grep -iq "^y" ;then
-	sudo apt-get install python-pip
-	pip install speedtest-cli
-	speedtest-cli
+if echo "$answer" | grep -iq "^n" ; then
+	exit
 fi
+sudo apt-get install -y python-pip
+pip install speedtest-cli
+speedtest-cli
