2c4c3b0f00ed97769c93907acb76279e6d3a59a5
Carmelo Santana <me@carmelosantana.com>
2017-03-08 00:09:54 -0500

    Updated version Cleaned up y/n

--- install-geekbench.sh
+++ install-geekbench.sh
@@ -2,9 +2,10 @@
 # Geekbench Installer
 echo -n "Install + run geekbench? (y|n)"
 read answer
-if echo "$answer" | grep -iq "^y" ; then
-	wget http://cdn.primatelabs.com/Geekbench-4.0.1-Linux.tar.gz
-	tar -zxvf Geekbench-4.0.1-Linux.tar.gz
-	cd build.pulse/dist/Geekbench-4.0.1-Linux/
-	./geekbench4
+if echo "$answer" | grep -iq "^n" ; then
+	exit
 fi
+wget http://cdn.primatelabs.com/Geekbench-4.0.4-Linux.tar.gz
+tar -zxvf Geekbench-4.0.4-Linux.tar.gz
+cd build.pulse/dist/Geekbench-4.0.4-Linux/
+./geekbench4
