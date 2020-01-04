f95e74ec5f56fc0557835165809f7484fe539774
Carmelo Santana <me@carmelosantana.com>
2020-01-04 16:15:59 -0500

    Cleaned up shellper.sh download
    Added shellper.sh error notices

--- linode/shellper.sh
+++ linode/shellper.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-# v0.2
+# v0.3
 # <UDF name="sudo_username" Label="New sudo user" default="deploy" />
 # <UDF name="sudo_password" Label="New sudo password"/>
 # <UDF name="shellper_branch" Label="Shellper Github branch" oneOf="master,dev" default="master"/>
 
-# dependencies
-SHELLPER_PATH="$(dirname $(pwd))/shellper/shellper.sh"
+SHELLPER_PATH="shellper/shellper.sh"
 if [ ! -f "$SHELLPER_PATH" ]; then
     if [ ! -n "$SHELLPER_BRANCH" ]; then
         SHELLPER_BRANCH="master"
     fi
-    CURRENT_DIR="$(pwd)"
-    cd ../        
     git clone https://github.com/carmelosantana/shellper -b "$SHELLPER_BRANCH"
-    cd "$CURRENT_DIR"
+    echo '● Git clone shellper.sh'
 fi
+
+if [ ! -f "$SHELLPER_PATH" ]; then
+    echo '● Error loading shellper.sh'
+    exit 1
+fi
+
 source "$SHELLPER_PATH"
+
 apt_update_upgrade
 setup_sudo_user "$SUDO_USERNAME" "$SUDO_PASSWORD"
