8bb03600397cffb94f7ffb8a1a133a9fb65af980
Carmelo Santana <me@carmelosantana.com>
2021-08-06 16:40:50 -0400

    Added installer

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Setup paths
+SHELLPER="shellper.sh"
+
+# Check + install dependencies
+for install in "rlwrap"; do
+    if ! $(command -v "$install") &>/dev/null; then
+        sudo apt update
+        sudo apt install "$install" -y
+    fi
+done
+
+# Download directly from github branch
+wget "https://raw.githubusercontent.com/carmelosantana/shellper/master/$SHELLPER"
+
+# Set permissions
+chmod +x "$SHELLPER"
+
+# Install
+mv "$SHELLPER" /usr/local/bin/shellper
+
+# We're done
+echo -n "Install complete."
